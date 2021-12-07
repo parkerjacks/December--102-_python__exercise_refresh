fe76bd7ca7fc79aed129efd88d967e998fa6724e
Park Jack <parker.jackson.hhs@gmail.com>
2021-12-06 19:04:30 -0500

    created a new file to practice class initiation formatting

---
+++ class-basics.py
@@ -0,0 +1,19 @@
+class car: 
+    def __init__(self, name, color): 
+        self.name = name 
+        self.color = color
+        
+
+class apartment: 
+    def __init__(self, name, squarefoot):
+            self.name = name
+            self.squarefoot = squarefoot
+        
+class human: 
+    def __init__(self, hairColor, age, residence):
+            self.hairColor = hairColor
+            self.age = age 
+            self.residence = residence
+
+
+        
