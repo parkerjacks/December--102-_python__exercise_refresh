2ca79d2474f6cf48de0c31df91f8db0e94af6a8c
Park Jack <parker.jackson.hhs@gmail.com>
2021-12-09 20:59:44 -0500

    created a simple loop where users can choose an input and count up to that output

---
+++ n_to_m.py
@@ -0,0 +1,9 @@
+n = int(input("Please type a number to begin count: "))
+m = int(input("Please type a number to end count: ")) 
+
+while n >= m: 
+    n = int(input("Please type a number to begin count: "))
+    m = int(input("Please type a number to end count: ")) 
+while n <= m: 
+    print(n) 
+    n += 1
