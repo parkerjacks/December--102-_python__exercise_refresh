d756fa4fd06b72b14edb78de148638aeaa969187
Park Jack <parker.jackson.hhs@gmail.com>
2021-12-10 23:41:53 -0500

    writing simple for loops including the range function

---
+++ for_loop_small.py
@@ -0,0 +1,28 @@
+""" 
+For these exercises, use a for loop.
+
+Print the numbers from 0 to 99
+
+
+Print the even numbers from 0 to 99
+
+
+Print the odd numbers from 0 to 99 
+
+"""
+#Print the numbers from 0 to 99
+
+for x in range(99 + 1): 
+    print(x) 
+
+
+#Print the even numbers from 0 to 99
+
+for x in range(99 + 1): 
+    if x % 2 == 0: 
+        print(x)
+
+# Print the odd numbers from 0 to 99 
+for x in range(99 + 1): 
+    if x % 2 == 1: 
+        print(x)
